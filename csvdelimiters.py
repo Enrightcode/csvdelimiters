c72b8bd4f7a12a3d79bbd68c758fd5dfc754b6fe
Enrightcode <130708596+Enrightcode@users.noreply.github.com>
2023-05-26 18:35:56 +0200

    Create csvdelimiters.py

---
+++ csvdelimiters.py
@@ -0,0 +1,17 @@
+with open ('10_02_us.csv', 'r') as f:
+  data = list(csv.DictReader(f, delimiter='\t'))
+primes = []
+  for number in range(2,99999):
+   if number % factor == 0:
+      break
+ else:
+  primes.append(number)
+  
+data = [row for row in data if int(row['postal code']} in primes and row['state code'] == 'MA']
+len(data)
+
+with open('10_02_ma_prime.csv','w') as f:
+  writer = csv.writer(f)
+  for row in data:
+    writer.writerow([row['place name'], row['county']]}
+    
